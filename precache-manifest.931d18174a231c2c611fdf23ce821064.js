f46a4afb5619150cdaa48291c0f1248543ba207e
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2020-05-13 14:23:40 +0300

    Updates

---
+++ precache-manifest.931d18174a231c2c611fdf23ce821064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237c264935d9fd59d269508423205884",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/index.html"
+  },
+  {
+    "revision": "48bdcc43672899ff435c",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "6263c7708e4786bcca89",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/static/css/main.8c87e368.chunk.css"
+  },
+  {
+    "revision": "48bdcc43672899ff435c",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/static/js/2.29de5731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/static/js/2.29de5731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6263c7708e4786bcca89",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/static/js/main.b173db04.chunk.js"
+  },
+  {
+    "revision": "07cabf0a677b6be32b79",
+    "url": "https://mindtwisted.github.io/fcc-javascript-calculator/static/js/runtime-main.700a0900.js"
+  }
+]);
