a50f6c614d4201a80e934a13a2bacdbb40d4eb5f
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2020-05-13 14:10:17 +0300

    calculation engine replace eval with custom logic;

--- src/helpers/CalculationEngine.ts
+++ src/helpers/CalculationEngine.ts
@@ -1,4 +1,5 @@
 import CalculationQueue from './CalculationQueue'
+import { isDivisionOperator, isMinusOperator, isMultiplyOperator, isPlusOperator } from './utils'
 
 const getTheNumberOfDecimalPlaces = (number: number): number => {
   const numbersAfterDecimal = String(number).match(/^[0-9]+\.([0-9]+)$/)
@@ -10,14 +11,65 @@ const getTheNumberOfDecimalPlaces = (number: number): number => {
   return 0
 }
 
+const processMultiplyAndDivision = (calculationArray: string[]): string[] => {
+  calculationArray = [...calculationArray]
+
+  while (calculationArray.includes('*') || calculationArray.includes('/')) {
+    for (let i = 1; i < calculationArray.length; i += 2) {
+      const current = calculationArray[i]
+      const isMultiplyOperation = isMultiplyOperator(current)
+      const isDivisionOperation = isDivisionOperator(current)
+
+      if (isMultiplyOperation || isDivisionOperation) {
+        const firstOperand = Number(calculationArray[i - 1])
+        const secondOperand = Number(calculationArray[i + 1])
+        const result = isMultiplyOperation ? firstOperand * secondOperand : firstOperand / secondOperand
+
+        calculationArray.splice(i - 1, 3, String(result))
+
+        break
+      }
+    }
+  }
+
+  return calculationArray
+}
+
+const processPlusAndMinus = (calculationArray: string[]): string[] => {
+  calculationArray = [...calculationArray]
+
+  while (calculationArray.includes('+') || calculationArray.includes('-')) {
+    for (let i = 1; i < calculationArray.length; i += 2) {
+      const current = calculationArray[i]
+      const isAddOperation = isPlusOperator(current)
+      const isSubtractionOperation = isMinusOperator(current)
+
+      if (isAddOperation || isSubtractionOperation) {
+        const firstOperand = Number(calculationArray[i - 1])
+        const secondOperand = Number(calculationArray[i + 1])
+        const result = isAddOperation ? firstOperand + secondOperand : firstOperand - secondOperand
+
+        calculationArray.splice(i - 1, 3, String(result))
+
+        break
+      }
+    }
+  }
+
+  return calculationArray
+}
+
 const CalculationEngine = {
   calculateQueue (calculationQueue: CalculationQueue): number {
     if (!calculationQueue.isValidForCalculation()) {
       throw Error('Invalid calculation queue has been provided')
     }
 
-    // eslint-disable-next-line no-eval
-    return eval(calculationQueue.getQueueAsArray().join(' '))
+    const calculationArray = calculationQueue.getQueueAsArray()
+    const arrayWithMultiplyAndDivisionProcessed = processMultiplyAndDivision(calculationArray)
+    const arrayWithPlusAndMinusProcessed = processPlusAndMinus(arrayWithMultiplyAndDivisionProcessed)
+
+    return Number(arrayWithPlusAndMinusProcessed[0])
   },
   formatOutput (result: number, numberOfDecimalPlaces = 4): string {
     if (getTheNumberOfDecimalPlaces(result) > numberOfDecimalPlaces) {
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,9 +1,12 @@
 export const isNumber = (value: string): boolean => /^[0-9]$/.test(value)
-export const isZero = (value: string): boolean => /^0$/.test(value)
 export const isOperator = (value: string): boolean => /^[+\-*=/]$/.test(value)
-export const isEqualOperator = (value: string): boolean => /^=$/.test(value)
-export const isMinusOperator = (value: string): boolean => /^-$/.test(value)
-export const isDot = (value: string): boolean => /^\.$/.test(value)
+export const isZero = (value: string): boolean => value === '0'
+export const isEqualOperator = (value: string): boolean => value === '='
+export const isMinusOperator = (value: string): boolean => value === '-'
+export const isPlusOperator = (value: string): boolean => value === '+'
+export const isMultiplyOperator = (value: string): boolean => value === '*'
+export const isDivisionOperator = (value: string): boolean => value === '/'
+export const isDot = (value: string): boolean => value === '.'
 export const isEmptyString = (value: string): boolean => value.length === 0
 export const isStringOfNumbers = (value: string): boolean => /^-?[0-9]+$/.test(value)
 export const isStringOfNumbersEndsWithDot = (value: string): boolean => /^-?[0-9]+\.$/.test(value)
